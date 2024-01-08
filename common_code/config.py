8ed0d99066594c166675324ed707c7f955b016d1
Arnaud Demion <arnaud.demion@hes-so.ch>
2024-01-08 12:25:35 +0100

    Added pydantic-settings for BaseSettings

--- common_code/config.py
+++ common_code/config.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import List
 from functools import lru_cache
-from pydantic-settings import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Environment(str, Enum):
