59195af54d348da0b57e4cc9603cff362aa59dfb
Henrique Marques Reis <henrique.marquesreis@he-arc.ch>
2024-03-11 11:48:20 +0100

    add test result model

--- common_code/common/models.py
+++ common_code/common/models.py
@@ -7,6 +7,7 @@ class FieldDescription(TypedDict):
     """
     Field description model
     """
+
     name: str
     type: List[FieldDescriptionType]
 
@@ -15,5 +16,28 @@ class ExecutionUnitTag(TypedDict):
     """
     Service tag model
     """
+
     name: ExecutionUnitTagName
     acronym: ExecutionUnitTagAcronym
+
+
+class TestResult(TypedDict):
+    """
+    Test result model
+    name: test name
+    result: test result (True/False)
+    """
+
+    name: str
+    result: bool
+
+
+class TestResultList(TypedDict):
+    """
+    Test result list model
+    tests_results: list of test results
+    tests_passed: True if all tests passed, False otherwise
+    """
+
+    results: List[TestResult]
+    tests_passed: bool
